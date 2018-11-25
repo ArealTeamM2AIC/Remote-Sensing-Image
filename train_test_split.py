400e95b7add855d3c1772fd04daab7e0968402c8
Hao LIU <liuhaoxdu@gmail.com>
2018-11-25 11:19:55 +0100

    random split

--- train_test_split.py
+++ train_test_split.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import glob
 import shutil
 import os
 
-
-# In[2]:
-
-
 src_dir = "NWPU-RESISC45"
 dst_dir = "RESISC45"
-
-
-# In[3]:
-
 
 train = '/train/'
 test = '/test/'
@@ -26,7 +15,7 @@ for cal in os.listdir(src_dir):
     images = []
     for jpgfile in glob.iglob(os.path.join("NWPU-RESISC45/"+cal, "*.jpg")):
         images.append(jpgfile)
-    images = sorted(images)
+    #images = sorted(images)
     
     direct_train = dst_dir+train+cal+"/" 
     direct_test = dst_dir+test+cal+"/"
@@ -37,13 +26,13 @@ for cal in os.listdir(src_dir):
     count = 0
     for jpgfile in images:
         #Split 400 to train
-        if count>=0 and count<400:
+        if count>=0 and count<150:
             shutil.copy(jpgfile, direct_train)
         #Split 50 to val
-        if count>=400 and count<450:
+        if count>=150 and count<300:
             shutil.copy(jpgfile, direct_val)
         #Split 250 to test
-        if count>=450 and count<700:
+        if count>=300 and count<700:
             shutil.copy(jpgfile, direct_test)
         count +=1
 
