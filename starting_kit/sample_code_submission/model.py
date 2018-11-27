35711657d2348be01bbc5594bffa455043a35413
RobinKD <robin.durae@gmail.com>
2018-11-28 00:08:51 +0100

    update starting kit

--- starting_kit/sample_code_submission/model.py
+++ starting_kit/sample_code_submission/model.py
@@ -9,8 +9,9 @@ from torchvision import transforms
 from sklearn.base import BaseEstimator
 from PIL import Image
 from matplotlib import cm
+from sklearn.tree import DecisionTreeClassifier
 
-class AlexNetModel(BaseEstimator):
+class AlexNetModelBis(BaseEstimator):
     def __init__(self, learning_rate=1e-3, nb_epoch=10, verbose=False, batch_size=32, n_class=13, use_cuda=False):
         super(AlexNetModel, self).__init__()
         self.nb_epoch = nb_epoch
@@ -135,6 +136,51 @@ class AlexNetModel(BaseEstimator):
         for i in range(pred.size(0)):
             res.append(inverted_dico[pred[i].item()])
         return np.asarray(res)
+
+    def save(self, path="./"):
+        pickle.dump(self, open(path + '_model.pickle', "wb"))
+
+    def load(self, path="./"):
+        modelfile = path + '_model.pickle'
+        if isfile(modelfile):
+            with open(modelfile, 'rb') as f:
+                self = pickle.load(f)
+            print("Model reloaded from: " + modelfile)
+        return self
+
+class BaselineModel(BaseEstimator):
+    def __init__(self):
+        """
+        Using DecisionTreeClassifier from sklearn as Baseline Model
+        """
+        super(DecisionTreeClassifier, self).__init__()
+        self.classifier = DecisionTreeClassifier()
+        self.num_train_samples=0
+        self.num_feat=1
+        self.num_labels=1
+        self.is_trained=False
+
+    def fit(self, X, y):
+        self.num_train_samples = X.shape[0]
+        if X.ndim>1: self.num_feat = X.shape[1]
+        print("FIT: dim(X)= [{:d}, {:d}]".format(self.num_train_samples, self.num_feat))
+        num_train_samples = y.shape[0]
+        if y.ndim>1: self.num_labels = y.shape[1]
+        print("FIT: dim(y)= [{:d}, {:d}]".format(num_train_samples, self.num_labels))
+        if (self.num_train_samples != num_train_samples):
+            print("ARRGH: number of samples in X and y do not match!")
+        self.is_trained=True
+        self.classifier.fit(X, y)
+
+    def predict(self, X):
+        num_test_samples = X.shape[0]
+        if X.ndim>1: num_feat = X.shape[1]
+        print("PREDICT: dim(X)= [{:d}, {:d}]".format(num_test_samples, num_feat))
+        if (self.num_feat != num_feat):
+            print("ARRGH: number of features in X does not match training data!")
+        print("PREDICT: dim(y)= [{:d}, {:d}]".format(num_test_samples, self.num_labels))
+        y = np.zeros([num_test_samples, self.num_labels])
+        return self.classifier.predict(X)
 
     def save(self, path="./"):
         pickle.dump(self, open(path + '_model.pickle', "wb"))
