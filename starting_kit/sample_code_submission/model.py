54dc394db41510b92cd0c61b30057f8061ebd6f8
RobinKD <robin.durae@gmail.com>
2018-12-03 19:18:59 +0100

    Update normal starting kit

--- starting_kit/sample_code_submission/model.py
+++ starting_kit/sample_code_submission/model.py
@@ -16,10 +16,33 @@ def requires_grad(p):
     return p.requires_grad
 
 
-class AlexNetModel(BaseEstimator):
-    def __init__(self, learning_rate=1e-3, nb_epoch = 10, batch_size = 32, verbose=False, use_cuda=False):
-        super(AlexNetModel, self).__init__()
-        
+class SimpleConvModel(nn.Module):
+    def __init__(self, out_channel_conv=16, linear_hidden_size=256, out_class=13):
+        super(SimpleConvModel, self).__init__()
+
+        self.out_channel_conv = out_channel_conv
+        self.linear_hidden_size = linear_hidden_size
+        self.out_class = out_class
+
+        self.seq1 = nn.Sequential(nn.Conv2d(3, self.out_channel_conv, (10,10), stride=(3,3)),
+                                  nn.MaxPool2d((5,5)),
+                                  nn.ReLU())
+
+        self.seq2 = nn.Sequential(nn.Linear(16 * 16 * 16, self.out_class),#self.linear_hidden_size),
+                                  #nn.ReLU(),
+                                  #nn.Linear(self.linear_hidden_size, self.out_class),
+                                  nn.Softmax(dim = 1)) 
+
+    def forward(self, x):
+        out1 = self.seq1(x)
+        out2 = out1.view(-1, 16*16*16)
+        out3 = self.seq2(out2)
+        return out3
+
+class BasicCNN(BaseEstimator):
+    def __init__(self, learning_rate=1e-3, nb_epoch = 10, batch_size = 32, verbose=False):
+        super(BasicCNN, self).__init__()
+
         if learning_rate is None:
             learning_rate = 1e-3
         if nb_epoch is None:
@@ -28,51 +51,29 @@ class AlexNetModel(BaseEstimator):
             batch_size = 32
         if verbose is None:
             verbose = False
-        if use_cuda is None:
-            use_cuda = False        
-        
-        self.verbose = verbose
+
         self.nb_epoch = nb_epoch
         self.batch_size = batch_size
-        
-        self.model_conv = torchvision.models.alexnet(pretrained=True)
-        
-        num_ftrs = self.model_conv.classifier[6].in_features
-        # convert all the layers to list and remove the last one
-        features = list(self.model_conv.classifier.children())[:-1]
-        ## Add the last layer based on the num of classes in our dataset
-        features.extend([nn.Linear(num_ftrs, 13)])
-        ## convert it into container and add it to our model class.
-        self.model_conv.classifier = nn.Sequential(*features)
-        
+        self.verbose = verbose
+
+        self.model_conv = SimpleConvModel()
+
         # Loss function
         self.criterion = nn.CrossEntropyLoss()
-        
+
         # Optimizer
-        self.optim = optim.SGD(list(filter(requires_grad, self.model_conv.parameters())), lr=learning_rate, momentum=0.9)
-        
-        self.use_cuda = use_cuda
-        
-        if self.use_cuda:
-            self.model_conv.cuda()
-            self.criterion.cuda()
-            
+        self.optim = optim.Adagrad(self.model_conv.parameters(), lr=1e-3, weight_decay=0.2)
+
         # Image transformation
-        input_shape = 224
         mean = [0.5, 0.5, 0.5]
         std = [0.5, 0.5, 0.5]
         scale = 360
-        input_shape = 224
         use_parallel = True
 
+
         self.data_transforms = transforms.Compose([
-            transforms.Resize(scale),
-            transforms.RandomResizedCrop(input_shape),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomVerticalFlip(),
-            transforms.RandomRotation(degrees=90),
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std)])
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)])
 
     def fit(self, X, y):
         '''
@@ -85,46 +86,33 @@ class AlexNetModel(BaseEstimator):
         X = self.process_data(X)
         n_sample = X.size(0)
         nb_batch = int(n_sample / self.batch_size)
-        
+
         y = self.process_label(y)
-        
+
         self.model_conv.train()
         for e in range(self.nb_epoch):
             sum_loss = 0
-            for i in range(nb_batch):
-                self.model_conv.zero_grad()
-                
-                start = i * self.batch_size
-                end = max((i + 1) * self.batch_size, n_sample)
-             
-                batch = X[start:end]
-                batch_lbl = y[start:end]
-                if self.use_cuda:
-                    batch = batch.cuda()
-                    batch_lbl = batch_lbl.cuda()
-                
-                out = self.model_conv(batch)
-                loss = self.criterion(out, batch_lbl)
-                
-                loss.backward()
-                self.optim.step()
-                
-                sum_loss += loss.item()
+            out = self.model_conv(X)
+            loss = self.criterion(out, y)
+
+            loss.backward()
+            self.optim.step()
+
+            sum_loss += loss.item()
             if self.verbose:
-                print("Epoch %d : loss = %f" % (e, sum_loss / nb_batch))
-            
-        
+                print("Epoch %d : loss = %f" % (e, sum_loss))
+
     def process_data(self, X):
         n_sample = X.shape[0]
         X = X.reshape(n_sample, 3, 256, 256)
-        res = torch.zeros(1,3,224,224)
+        res = torch.zeros(1,3,256,256)
         for i in range(n_sample):
             x = np.moveaxis(X[i], 0, -1)
             img = Image.fromarray((x*255).astype('uint8'))
             t = self.data_transforms(img).unsqueeze(0)
             res = torch.cat((res,t))
         return res[1:]
-    
+
     def process_label(self, y):
         self.label_dico = {}
         res = torch.zeros(1)
@@ -135,7 +123,7 @@ class AlexNetModel(BaseEstimator):
             l = torch.Tensor([self.label_dico[l]])
             res = torch.cat((res, l))
         return res[1:].type(torch.long) 
-        
+
     def predict(self, X):
         '''
             param X: numpy.ndarray
@@ -146,18 +134,12 @@ class AlexNetModel(BaseEstimator):
                 of float with shape (num_sample, num_class) ?
                 of string with shape (num_sample) ?
         '''
-        inverted_dico = {v:k for k,v in self.label_dico.items()}
+        # inverted_dico = {v:k for k,v in self.label_dico.items()}
         self.model_conv.eval()
         X = self.process_data(X)
-        
-        if self.use_cuda:
-            X = X.cuda()
-            
-        pred = self.model_conv(X).argmax(dim=1).detach().cpu()
-        res = []
-        for i in range(pred.size(0)):
-            res.append(inverted_dico[pred[i].item()])
-        return np.asarray(res)
+
+        pred = self.model_conv(X).argmax(dim=1)
+        return pred
 
     def save(self, path="./"):
         pickle.dump(self, open(path + '_model.pickle', "wb"))
